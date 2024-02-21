b11c8bc1f78423b018c21426e81b0f2cfa09be46
inesion <toxiceu1337@gmail.com>
2024-02-21 18:31:13 +0100

    ooowweeeee

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('does the cicd works? let\'s find out!')
+  res.send('updated letsgo')
 })
 
 app.listen(port, () => {
